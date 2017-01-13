5c1092705da3410812ec26544c705536cb5abfd7
Daniel <Daniel@DESKTOP-PC0MCKN>
2017-01-13 20:28:26 +0200

    Al 4lea commit

--- EGit Tutorial/src/Example.java
+++ EGit Tutorial/src/Example.java
@@ -4,7 +4,6 @@ public class Example {
 	public static void main(String[] args)
 	{
 		System.out.println("Primul commit!");
-		System.out.println("Al doilea commit!!!");
 		System.out.println("Al treilea commit!!!!");
 	}
 	
